bab3c82e96368c321d9794ed288235fe53046975
Ivorysanna <violetta.pyralov@web.de>
2020-06-22 23:37:49 +0200

    Aufgabe 09 angelegt

---
+++ Aufgaben/09/js/anmelden.ts
@@ -0,0 +1,23 @@
+namespace AnmeldeSeite {
+    document.querySelector("#absendeButton")?.addEventListener("click", anmeldung);
+
+    async function anmeldung(): Promise<void> {
+        //Erstellt eine neue Formdata aus dem ersten Element im Formulararray
+        let formData: FormData = new FormData(document.forms[0]);
+
+        //Meine URL auf Heroku
+        let url: URL = new URL("https://gissose.herokuapp.com");
+
+
+        //Iteriert über die Einträge der FormData
+        for (let entry of formData) {
+            //Fügt die FormData entrys zur URL hinzu als Suchparameter hinzu
+            url.searchParams.append(entry[0], entry[1].toString());
+        }
+        
+        //Schickt Anfrage an die URL mit den Suchparamtern
+        let response: Response = await fetch(url.href);
+        console.log(response);
+    }
+
+}
---
+++ Aufgaben/09/js/server.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A08Server = void 0;
+const Http = require("http");
+var A08Server;
+(function (A08Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    //Server wird erstellt mit createServer
+    let server = Http.createServer();
+    //fügt Request-Listener hinzu
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    //Server beginnt auf Requests zu hören
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        //Setzen von Metadaten der Antowrt
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Inhalt von Response
+        _response.write(_request.url);
+        console.log(_request.url);
+        //Beendet Response und schickt sie ab
+        _response.end();
+    }
+})(A08Server = exports.A08Server || (exports.A08Server = {}));
+//# sourceMappingURL=server.js.map
