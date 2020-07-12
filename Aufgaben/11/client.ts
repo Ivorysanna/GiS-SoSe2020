4c2f35f86c8ead1346e3aa6d2b50588294b2d925
Ivorysanna <violetta.pyralov@web.de>
2020-07-12 16:00:51 +0200

    kp was hier war

--- Aufgaben/11/client.ts
+++ Aufgaben/11/client.ts
@@ -3,10 +3,48 @@ namespace Client{
     document.querySelector("#serverdaten")?.addEventListener("click", serverdatenFunktion);
 
     function absendenFunktion(){
+         //Erstellt eine neue Formdata aus dem ersten Element im Formulararray
+         let formData: FormData = new FormData(document.forms[0]);
 
+         //Meine URL auf Heroku html
+         let url: URL = new URL("https://gissose.herokuapp.com/write");
+ 
+ 
+         //Iteriert über die Einträge der FormData
+         for (let entry of formData) {
+             //Fügt die FormData entrys zur URL hinzu als Suchparameter hinzu
+             url.searchParams.append(entry[0], entry[1].toString());
+         }
+ 
+         //Schickt Anfrage an die URL mit den Suchparamtern
+         fetch(url.href)
+             .then(function (response) {
+                 return response.text();
+             }).then(function (data) {
+                 document.querySelector("#antwort")!.innerHTML = data;
+             });
     }
 
     function serverdatenFunktion(){
+         //Erstellt eine neue Formdata aus dem ersten Element im Formulararray
+         let formData: FormData = new FormData(document.forms[0]);
 
+         //Meine URL auf Heroku html
+         let url: URL = new URL("https://gissose.herokuapp.com/read");
+ 
+ 
+         //Iteriert über die Einträge der FormData
+         for (let entry of formData) {
+             //Fügt die FormData entrys zur URL hinzu als Suchparameter hinzu
+             url.searchParams.append(entry[0], entry[1].toString());
+         }
+ 
+         //Schickt Anfrage an die URL mit den Suchparamtern
+         fetch(url.href)
+             .then(function (response) {
+                 return response.text();
+             }).then(function (data) {
+                 document.querySelector("#antwort")!.innerHTML = data;
+             });
     }
 }
