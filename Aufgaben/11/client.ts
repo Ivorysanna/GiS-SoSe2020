18ce3e04b74949d6b077bfa90b1740e1fcf15259
Ivorysanna <violetta.pyralov@web.de>
2020-07-12 15:16:15 +0200

    Erstellen von  client mit function für buttons

---
+++ Aufgaben/11/client.ts
@@ -0,0 +1,14 @@
+import { createPublicKey } from "crypto";
+
+namespace Client{
+    document.querySelector("#absenden")?.addEventListener("click", absendenFunktion);
+    document.querySelector("#serverdaten")?.addEventListener("click", serverdatenFunktion);
+
+    function absendenFunktion(){
+
+    }
+
+    function serverdatenFunktion(){
+
+    }
+}
